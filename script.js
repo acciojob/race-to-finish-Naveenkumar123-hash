32413812c05bf48cb84117f873f9253e4913ccde
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-06-26 21:30:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,3 +2,26 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
+let a=new Promise((resolve,reject)=>{
+	setTimeOut(()
+		resolve()},1000
+)
+let b=new Promise((resolve,reject)=>{
+	setTimeOut(()
+		resolve()},2000
+)
+let c=new Promise((resolve,reject)=>{
+	setTimeOut(()
+		resolve()},3000
+)
+let d=new Promise((resolve,reject)=>{
+	setTimeOut(()
+		resolve()},4000
+)
+let e=new Promise((resolve,reject)=>{
+	setTimeOut((data)
+		resolve()},5000
+)
+promises=[a,b,c,d,e];
+const res=document.getElementById("output");
+res.any[a,b,c,d,e];
