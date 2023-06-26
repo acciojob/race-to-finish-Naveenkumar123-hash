cbf6dfdba03905996f0f6955fca2f96ff245f44d
Naveenkumar123-hash <120244421+Naveenkumar123-hash@users.noreply.github.com>
2023-06-26 21:44:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,26 +2,29 @@ window.promises = [];
 
 // Do not change the code above this
 // add your promises to the array `promises`
-let a=new Promise((resolve,reject)=>{
-	setTimeOut(()
-		resolve()},1000
-)
-let b=new Promise((resolve,reject)=>{
-	setTimeOut(()
-		resolve()},2000
-)
-let c=new Promise((resolve,reject)=>{
-	setTimeOut(()
-		resolve()},3000
-)
-let d=new Promise((resolve,reject)=>{
-	setTimeOut(()
-		resolve()},4000
-)
-let e=new Promise((resolve,reject)=>{
-	setTimeOut((data)
+let a=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("a")},1000
+)})
+let b=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("b")},2000
+)})
+let c=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("c")},3000
+)})
+let d=new Promise(resolve=>{
+	setTimeout(()=>{
+		resolve("d")},4000
+)})
+let e=new Promise(resolve=>{
+	setTimeout((e)=>{
 		resolve()},5000
-)
-promises=[a,b,c,d,e];
-const res=document.getElementById("output");
-res.any[a,b,c,d,e];
+)})
+window.promises=[a,b,c,d,e];
+let x=Promise.any([a,b,c,d,e]);
+x.then((data)=>{
+	let res=document.getElementById("output");
+	res.innerText=data;
+})
